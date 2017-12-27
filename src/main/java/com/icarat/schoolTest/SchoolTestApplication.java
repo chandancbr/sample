40b384d4ec2aaa0ab62eb8b03a4914fab8605e37
Gurudath <gurudathg121@gmail.com>
2017-12-27 15:02:15 +0530

    first commit

---
+++ src/main/java/com/icarat/schoolTest/SchoolTestApplication.java
@@ -0,0 +1,12 @@
+package com.icarat.schoolTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolTestApplication.class, args);
+	}
+}
